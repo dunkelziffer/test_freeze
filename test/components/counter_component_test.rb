97d49af656bb225b31b3640387808a3140cabf67
Klaus Weidinger <weidkl@gmx.de>
2026-04-11 23:49:25 +0200

    Better test

--- test/components/counter_component_test.rb
+++ test/components/counter_component_test.rb
@@ -7,17 +7,18 @@ class CounterComponentTest < ActiveSupport::TestCase
   def test_renders_counter_structure
     render_inline(CounterComponent.new(initial_value: 5))
 
-    html = rendered_content
+    assert_selector ".counter"
+    assert_selector "button[data-element='counter:decrement']", text: "−"
+    assert_selector "span[data-element='counter:value']", text: "5"
+    assert_selector "button[data-element='counter:increment']", text: "+"
 
-    assert_match %r{<div[^>]*class="counter"[^>]*>}, html
-    assert_match %r{<button[^>]*data-element="counter:decrement"[^>]*>−</button>}, html
-    assert_match %r{<span[^>]*data-element="counter:value"[^>]*>5</span>}, html
-    assert_match %r{<button[^>]*data-element="counter:increment"[^>]*>\+</button>}, html
+    data = JSON.parse(page.find(".counter")["up-data"])
+    assert_equal 5, data["initialValue"]
   end
 
   def test_defaults_to_zero
     render_inline(CounterComponent.new)
 
-    assert_match %r{<span[^>]*data-element="counter:value"[^>]*>0</span>}, rendered_content
+    assert_selector "span[data-element='counter:value']", text: "0"
   end
 end
