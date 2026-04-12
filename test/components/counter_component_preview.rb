57ea3bbaf1451613956b22d417ce69ba90730f01
Klaus Weidinger <weidkl@gmx.de>
2026-04-12 16:48:09 +0200

    Some preview tags

--- test/components/counter_component_preview.rb
+++ test/components/counter_component_preview.rb
@@ -1,4 +1,18 @@
 class CounterComponentPreview < ViewComponent::Preview
+  # A counter with custom initial value
+  # ---
+  # You probably don't want to use this component for real,
+  # because it doesn't persist the counter value anywhere.
+  #
+  # @label Custom
+  # @param initial_value [Integer]
+  def custom(initial_value: 42)
+    render CounterComponent.new(initial_value: initial_value)
+  end
+
+  # @!group Initial Values
+
+  # @label Default
   def zero
     render CounterComponent.new
   end
@@ -10,4 +24,6 @@ class CounterComponentPreview < ViewComponent::Preview
   def negative_one
     render CounterComponent.new(initial_value: -1)
   end
+
+  # @!endgroup
 end
