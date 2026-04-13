302e0c8b96c51709b84a5c0cfa7c6a53061a7e03
Klaus Weidinger <weidkl@gmx.de>
2026-04-13 18:17:04 +0200

    Create ApplicationComponent

--- app/components/counter_component.rb
+++ app/components/counter_component.rb
@@ -1,4 +1,4 @@
-class CounterComponent < ViewComponent::Base
+class CounterComponent < ApplicationComponent
   def initialize(initial_value: 0, label: false)
     @initial_value = initial_value
     @label = label
