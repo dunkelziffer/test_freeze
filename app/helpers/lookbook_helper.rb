d6b9f6fcf52f8f1db050a8c7fb1003d68d06a49b
Klaus Weidinger <weidkl@gmx.de>
2026-04-13 12:56:58 +0200

    Showcase preview usage in tests

--- app/helpers/lookbook_helper.rb
+++ app/helpers/lookbook_helper.rb
@@ -1,6 +1,24 @@
 module LookbookHelper
-  def render_preview_scenario(preview, scenario)
-    render_args = preview.render_args(scenario.name, params: {})
+  def render_preview_scenario(preview_entity_or_name, scenario_entity_or_name, params: {})
+    preview = case preview_entity_or_name
+    when Lookbook::PreviewEntity
+      preview_entity_or_name
+    when String, Symbol
+      Lookbook.previews.find { |p| p.lookup_path == preview_entity_or_name.to_s }
+    else
+      raise ArgumentError, "Expected a Lookbook::PreviewEntity or a String/Symbol lookup path"
+    end
+
+    scenario_name = case scenario_entity_or_name
+    when Lookbook::ScenarioEntity
+      scenario_entity_or_name.name
+    when String, Symbol
+      scenario_entity_or_name.to_s
+    else
+      raise ArgumentError, "Expected a Lookbook::ScenarioEntity or a String/Symbol scenario name"
+    end
+
+    render_args = preview.render_args(scenario_name, params: params)
 
     # https://github.com/lookbook-hq/lookbook/blob/main/app/views/lookbook/previews/preview.html.erb
     if render_args[:component]
--- test/components/counter_component_test.rb
+++ test/components/counter_component_test.rb
@@ -1,8 +1,9 @@
 require "test_helper"
 require "view_component/test_helpers"
 
-class CounterComponentTest < ActiveSupport::TestCase
+class CounterComponentTest < ActionView::TestCase
   include ViewComponent::TestHelpers
+  include LookbookHelper
 
   def test_renders_counter_structure
     render_inline(CounterComponent.new(initial_value: 5))
@@ -18,6 +19,21 @@ class CounterComponentTest < ActiveSupport::TestCase
 
   def test_defaults_to_zero
     render_inline(CounterComponent.new)
+
+    assert_selector "span[data-element='counter:value']", text: "0"
+  end
+
+  # https://viewcomponent.org/guide/previews.html#previews-as-test-cases
+  def test_render_view_component_preview
+    render_preview(:zero)
+
+    assert_selector "span[data-element='counter:value']", text: "0"
+  end
+
+  # See `app/helpers/lookbook_helper.rb`.
+  def test_render_lookbook_preview
+    render_preview_scenario("counter", "zero")
+    @page = Capybara.string(rendered)
 
     assert_selector "span[data-element='counter:value']", text: "0"
   end
