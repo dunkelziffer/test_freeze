95eacf8359eb1bb31ffcdddd92a9f9a9f2d363c9
Klaus Weidinger <weidkl@gmx.de>
2026-08-12 14:21:26 +0200

    frozen:ui

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module TestFreeze
     # config.eager_load_paths << Rails.root.join("extras")
 
     # frozen:md
-    config.assets.paths << Rails.root.join("content")
+    # config.assets.paths << Rails.root.join("content")
     config.action_dispatch.rescue_responses["Decant::FileNotFound"] = :not_found
 
     # frozen:db
@@ -39,5 +39,14 @@ module TestFreeze
       g.helper nil
     end
     config.active_storage.draw_routes = true
+
+    # frozen:ui
+    config.i18n.available_locales = [ :en, :de ]
+    config.view_component.parent_class = "ApplicationComponent"
+    config.view_component.generate.preview = true
+    config.view_component.generate.preview_path = "test/components"
+    config.view_component.generate.locale = true
+    config.view_component.previews.default_layout = "component_preview"
+    config.asset_path = "/assets"
   end
 end
